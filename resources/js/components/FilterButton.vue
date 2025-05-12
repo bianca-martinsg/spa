588df7a7f3607e376246fbc3b5d45ac6f6ad7130
Bianca <biancamartinsgonc@gmail.com>
2025-05-12 01:52:29 -0300

    Add FilterButton component for toggling filters with dynamic styling and event emission

---
+++ resources/js/components/FilterButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="btn btn-light filter-button ms-2"
+        :class="{ 'btn-primary': isFiltersApplied, 'btn-light': !isFiltersApplied }" aria-label="Filtrar"
+        @click="toggleFilter">
+        <i :class="isFiltersApplied ? 'bi bi-funnel-fill' : 'bi bi-funnel'"></i>
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        isFiltersApplied: {
+            type: Boolean,
+            required: true
+        }
+    },
+    methods: {
+        toggleFilter() {
+            this.$emit('toggle-filter');
+        }
+    }
+};
+</script>
+
+<style scoped>
+.btn-light {
+    background-color: transparent;
+    border: 1px solid #DEE2E6;
+}
+</style>
