9d153a208598aeab7c9b55c550d5dcd4d9b3643c
Bianca <biancamartinsgonc@gmail.com>
2025-05-12 01:50:29 -0300

    Refactor HTML structure and update styles in app and welcome views for consistency and improved readability.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SPA Laravel 6.2</title>
+    <title>Produtos</title>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <script src="{{ mix('js/app.js') }}" defer></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
 </head>
+
 <body>
     <div id="app"></div>
 </body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Produtos</title>
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 </head>
+
 <body>
   <div id="app"></div>
   <script src="{{ mix('js/app.js') }}"></script>
 </body>
+
 </html>
